91d2aab51c07010948d07074650ea950ef142b25
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-02-17 08:39:44 +0530

    cleaned code in LogInController

--- course_reg_system/src/controller/LogInController.java
+++ course_reg_system/src/controller/LogInController.java
@@ -5,7 +5,6 @@ import dto.StudentDto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
