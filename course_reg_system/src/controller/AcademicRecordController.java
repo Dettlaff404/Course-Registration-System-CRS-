1d69597d22ddafefc2613054bbcb803b60c4746f
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-02-18 00:48:55 +0530

    Added method to show completed course data in the tblGradedCourses in AcademicRecord view

--- course_reg_system/src/controller/AcademicRecordController.java
+++ course_reg_system/src/controller/AcademicRecordController.java
@@ -1,10 +1,15 @@
 package controller;
 
+import java.util.ArrayList;
+
 import dto.EnrollmentDto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import service.ServiceFactory;
 import service.custom.AcademicRecordService;
 
@@ -46,10 +51,25 @@ public class AcademicRecordController {
 
     public void initialize(String student_id) throws Exception {
         this.student_id = student_id;
-        
-        setLabels();
-        System.out.println(academicRecordService.searchCompletedCourseByStudent(student_id));
 
+        setLabels();
+
+        setTable();
+        colCourseID.setCellValueFactory(new PropertyValueFactory<>("course_id"));
+        colCourse.setCellValueFactory(new PropertyValueFactory<>("course_name"));
+        colSemester.setCellValueFactory(new PropertyValueFactory<>("semester"));
+        colGrade.setCellValueFactory(new PropertyValueFactory<>("grade"));
+        colGrade.setStyle("-fx-alignment: CENTER;");
+        colSemester.setStyle("-fx-alignment: CENTER;");
+
+    }
+
+    public void setTable() throws Exception {
+        ArrayList<EnrollmentDto> completedCourses = academicRecordService.searchCompletedCourseByStudent(student_id);
+
+        ObservableList<EnrollmentDto> observableArrayList = FXCollections.observableArrayList();
+        observableArrayList.addAll(completedCourses);
+        tblGradedCourses.setItems(observableArrayList);
     }
 
     public void setLabels() throws Exception {
